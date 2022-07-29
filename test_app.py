691a3c659207a9e26f32832b30c6609dd6719f9b
Raytene Han <raytenehan@gmail.com>
2022-07-29 12:37:37 -0700

    added tests for homepage route

--- test_app.py
+++ test_app.py
@@ -23,8 +23,12 @@ class BoggleAppTestCase(TestCase):
 
         with self.client as client:
             response = client.get('/')
-            ...
+            # ...
+            html = response.get_data(as_text = True)
             # test that you're getting a template
+
+            self.assertEqual(response.status_code, 200)
+            self.assertIn('<table class="board">', html)
 
     def test_api_new_game(self):
         """Test starting a new game."""
